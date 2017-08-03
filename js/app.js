719202fb25cf89c21433f6d1115a4705f87c4c3f
Sai Somanath.K <k.saisomanath@gmail.com>
2017-08-03 18:45:26 +0530

    Activate button operations

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 $(document)
     .ready(function() {
+        var inChar = '';
+        var ans = '';
+        var log = '';
 
+        // Function to take the input on the click of a button
+        $("button")
+            .click(function() {
+                inChar = $(this)
+                    .attr("value");
+                console.log(inChar);
+                $("#log")
+                    .html(inChar);
+                $("#answer")
+                    .text(inChar);
+
+            });
     });
