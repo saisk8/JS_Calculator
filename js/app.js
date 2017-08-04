78e32048dda8a09749db19f9f078c9aa395da30d
Sai Somanath.K <k.saisomanath@gmail.com>
2017-08-04 14:21:47 +0530

    Decimal calulator

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ $(document)
         var ans = '';
         var log = '';
         var exp = '';
-        var index = 0;
         var last = '';
 
         // Function to take the input on the click of a button
@@ -14,6 +13,11 @@ $(document)
                     .attr("value");
                 //console.log(inChar);
                 if (isNaN(inChar)) {
+                    if (last === '=') {
+                        log = ans;
+                        ans = '';
+                    }
+
                     if (inChar === "ac") {
                         log = '';
                         exp = '';
@@ -22,26 +26,31 @@ $(document)
 
                     if (inChar === 'ce') {
                         var i = -1;
+                        //console.log(last);
                         switch (last) {
                             case '*':
                                 i = log.indexOf('x');
-                                if (index != -1)
-                                    log = log.substring(index);
-                                exp.slice(0, -1);
+                                if (i != -1)
+                                    log = log.substring(0, i);
+                                exp = exp.substring(0, exp.length - 1);
+                                console.log(exp);
                                 break;
                             case '/':
                                 i = log.indexOf('&divide;');
-                                if (index != -1)
-                                    log = log.substring(index);
-                                exp.slice(0, -1);
+                                if (i != -1)
+                                    log = log.substring(0, i);
+                                exp = exp.substring(0, exp.length - 1);
+                                console.log(exp);
                                 break;
                             case '=':
                                 log = '';
                                 exp = '';
                                 break;
                             default:
-                                log.slice(0, -1);
-                                exp.slice(0, -1);
+                                log = log.substring(0, log.length - 1);
+                                console.log(log);
+                                exp = exp.substring(0, exp.length - 1);
+                                console.log(exp);
                         }
                         ans = '';
                     }
@@ -49,21 +58,24 @@ $(document)
                     if (inChar === '*') {
                         last = inChar;
                         log += 'x';
-                        index += 1;
                         exp += inChar;
                     }
 
                     if (inChar === '/') {
                         last = inChar;
                         log += "&divide;";
-                        index += 1;
                         exp += inChar;
                     }
 
                     if (inChar === '=') {
                         last = inChar;
                         ans = eval(exp);
-                        log = ans;
+                    }
+
+                    if (inChar === '+' || inChar === '-' || inChar === '%') {
+                        last = inChar;
+                        log += inChar;
+                        exp += inChar;
                     }
                 } else {
                     last = inChar;
